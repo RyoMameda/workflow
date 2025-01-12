06c04e33a0b693a96ef11c09e0bbf18fad1fd46f
RyoMameda <128974787+RyoMameda@users.noreply.github.com>
2025-01-12 21:18:51 +0900

    Update 02_readsQC.sh

--- 02_readsQC.sh
+++ 02_readsQC.sh
@@ -9,8 +9,8 @@ for fq in *_1.fastq.gz; do
     f=${fq%_?.fastq.gz}
     echo ${f}
     time fastp -i ${f}_1.fastq.gz -I ${f}_2.fastq.gz -o ${f}_1_trim.fastq.gz -O ${f}_2_trim.fastq.gz -q 20 -h ${f}_fastp.html -w ${threads} -t 1 -T 1
-    mv *_?.fastq.gz raw_fq
-    mv *_?_trim.fastq.gz trim_fq
-    mv *.html fastp_html
+    mv ${f}_?.fastq.gz raw_fq
+    mv ${f}_?_trim.fastq.gz trim_fq
+    mv ${f}.html fastp_html
 done
 
