973531f2bc544fd5255e27750108a3b4d3eae965
RyoMameda <128974787+RyoMameda@users.noreply.github.com>
2025-01-12 21:58:45 +0900

    Update 03_assembly.sh

--- 03_assembly.sh
+++ 03_assembly.sh
@@ -4,8 +4,8 @@ threads=10
 fqdir=trim_fq
 
 for tfq in ${fqdir}/*_1_trim.fastq.gz; do
-    f=${tfq%_1_trim.fastq.gz}
+    f=${$(basename "$tfq")%_1_trim.fastq.gz}
     echo "${f} assembly"
-    time megahit -1 ${f}_1_trim.fastq.gz -2 ${f}_2_trim.fastq.gz -o contig${f} -t ${threads}
+    time megahit -1 ${fqdir}/${f}_1_trim.fastq.gz -2 ${fqdir}/${f}_2_trim.fastq.gz -o contig${f} -t ${threads}
     seqkit stats -a -T contig${f}/final.contigs.fa > contig${f}_statics.tsv
 done
