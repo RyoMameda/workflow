91a10d69a129b998db10f07380735fab1475c450
RyoMameda <128974787+RyoMameda@users.noreply.github.com>
2025-01-12 10:51:25 +0900

    Update 01_retrieval_reads.sh

--- 01_retrieval_reads.sh
+++ 01_retrieval_reads.sh
@@ -6,6 +6,6 @@ threads=10
 prefetch --option-file SRR_Acc_list.txt
 while IFS= read -r line; do
     echo "$line"
-    time fasterq-dump "$line".sra -e ${threads} -p -S
+    time fasterq-dump "$line"/*.sra -e ${threads} -p -S
     pigz *.fastq
-done
+done < SRR_Acc_list.txt
